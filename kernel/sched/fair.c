4c91882a4535aa7b71bd6c696859c9fd2c45d765
Peter Zijlstra <peterz@infradead.org>
2018-02-13 13:37:28 +0000

    sched/fair: Do not migrate due to a sync wakeup on exit
    When a task exits, it notifies the parent that it has exited. This is a
    sync wakeup and the exiting task may pull the parent towards the wakers
    CPU. For simple workloads like using a shell, it was observed that the
    shell is pulled across nodes by exiting processes. This is daft as the
    parent may be long-lived and properly placed. This patch special cases a
    sync wakeup on exit to avoid pulling tasks across nodes. Testing on a range
    of workloads and machines showed very little differences in performance
    although there was a small 3% boost on some machines running a shellscript
    intensive workload (git regression test suite).
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Giovanni Gherdovich <ggherdovich@suse.cz>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Matt Fleming <matt@codeblueprint.co.uk>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20180213133730.24064-5-mgorman@techsingularity.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8549,7 +8549,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	int new_cpu = prev_cpu;
 	int want_affine = 0;
 	int want_energy = 0;
-	int sync = wake_flags & WF_SYNC;
+	int sync = (wake_flags & WF_SYNC) && !(current->flags & PF_EXITING);
 	int high_cap_cpu =
 	     cpu_rq(cpu)->rd->mid_cap_orig_cpu != -1 ?
 	     cpu_rq(cpu)->rd->mid_cap_orig_cpu :
