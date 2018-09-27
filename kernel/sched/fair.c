6ddbb7ae89516f1dd3f73822a388704c77119c37
Patrick Bellasi <patrick.bellasi@arm.com>
2018-09-27 17:58:58 +0100

    BACKPORT: ANDROID: sched/core: Move SchedTune task API into UtilClamp wrappers
    The main SchedTune API calls realted to task tuning attributes are now
    wrapped by more generic and mainlinish UtilClamp calls.
    
    The new APIs are:
    
     - uclamp_task(p)               <= boosted_task_util(p)
     - uclamp_boosted(p)            <= schedtune_task_boost(p) > 0
     - uclamp_latency_sensitive(p)  <= schedtune_prefer_idle(p)
    
    Let's provide also an implementation of the same API based on the new
    uclamp.uclamp_latency_sensitive flag.
    
    Bug: 120440300
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    [Modified the patch to use uclamp.latency_sensitive instead mainline
    attributes]
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: Ib1a6902e1c07a82a370e36bf1776d895b7528cbc
    Signed-off-by: Quentin Perret <qperret@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3867,7 +3867,7 @@ static inline unsigned long _task_util_est(struct task_struct *p)
 	return max(ue.ewma, (ue.enqueued & ~UTIL_AVG_UNCHANGED));
 }
 
-static inline unsigned long task_util_est(struct task_struct *p)
+unsigned long task_util_est(struct task_struct *p)
 {
 	return max(task_util(p), _task_util_est(p));
 }
@@ -6610,8 +6610,7 @@ schedtune_cpu_margin_with(unsigned long util, int cpu, struct task_struct *p)
 	return margin;
 }
 
-static inline long
-schedtune_task_margin(struct task_struct *task)
+long schedtune_task_margin(struct task_struct *task)
 {
 	int boost = schedtune_task_boost(task);
 	unsigned long util;
@@ -6634,24 +6633,7 @@ schedtune_cpu_margin_with(unsigned long util, int cpu, struct task_struct *p)
 	return 0;
 }
 
-static inline int
-schedtune_task_margin(struct task_struct *task)
-{
-	return 0;
-}
-
 #endif /* CONFIG_SCHED_TUNE */
-
-static inline unsigned long
-boosted_task_util(struct task_struct *task)
-{
-	unsigned long util = task_util_est(task);
-	long margin = schedtune_task_margin(task);
-
-	trace_sched_boost_task(task, util, margin);
-
-	return util + margin;
-}
 
 static struct sched_group *
 find_idlest_group(struct sched_domain *sd, struct task_struct *p, int this_cpu);
@@ -7120,7 +7102,7 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 
 static inline int task_fits_capacity(struct task_struct *p, long capacity)
 {
-	return capacity * 1024 > boosted_task_util(p) * capacity_margin;
+	return capacity * 1024 > uclamp_task(p) * capacity_margin;
 }
 
 static int start_cpu(bool boosted)
@@ -7133,7 +7115,7 @@ static int start_cpu(bool boosted)
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				   bool boosted, bool prefer_idle)
 {
-	unsigned long min_util = boosted_task_util(p);
+	unsigned long min_util = uclamp_task(p);
 	unsigned long target_capacity = ULONG_MAX;
 	unsigned long min_wake_util = ULONG_MAX;
 	unsigned long target_max_spare_cap = 0;
@@ -7507,7 +7489,7 @@ static inline struct energy_env *get_eenv(struct task_struct *p, int prev_cpu)
 	 * util for group utilization calculations
 	 */
 	eenv->util_delta = task_util_est(p);
-	eenv->util_delta_boosted = boosted_task_util(p);
+	eenv->util_delta_boosted = uclamp_task(p);
 
 	cpumask_and(&cpumask_possible_cpus, &p->cpus_allowed, cpu_online_mask);
 	eenv->max_cpu_count = cpumask_weight(&cpumask_possible_cpus);
@@ -7578,7 +7560,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 				break;
 		}
 	} else {
-		int boosted = (schedtune_task_boost(p) > 0);
+		int boosted = (uclamp_boosted(p) > 0);
 		int prefer_idle;
 
 		/*
@@ -7587,7 +7569,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 		 * all if(prefer_idle) blocks.
 		 */
 		prefer_idle = sched_feat(EAS_PREFER_IDLE) ?
-				(schedtune_prefer_idle(p) > 0) : 0;
+				(uclamp_latency_sensitive(p) > 0) : 0;
 
 		eenv->max_cpu_count = EAS_CPU_BKP + 1;
 
@@ -7673,7 +7655,7 @@ static inline int wake_energy(struct task_struct *p, int prev_cpu,
 		 * Force prefer-idle tasks into the slow path, this may not happen
 		 * if none of the sd flags matched.
 		 */
-		if (schedtune_prefer_idle(p) > 0 && !sync)
+		if (uclamp_latency_sensitive(p) > 0 && !sync)
 			return false;
 	}
 	return true;
