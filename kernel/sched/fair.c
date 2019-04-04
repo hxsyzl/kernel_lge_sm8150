dea69335db52b4ddf1390e668409bdfdfc9d61ce
Patrick Bellasi <patrick.bellasi@arm.com>
2019-04-04 10:24:43 +0100

    ANDROID: sched/tune: Move SchedTune cpu API into UtilClamp wrappers
    The SchedTune CPU boosting API is currently used from sugov_get_util()
    to get the boosted utilization and to pass it into schedutil_cpu_util().
    
    When UtilClamp is in use instead we call schedutil_cpu_util() by
    passing in just the CFS utilization and the clamping is done internally
    on the aggregated CFS+RT utilization for FREQUENCY_UTIL calls.
    
    This asymmetry is not required moreover, schedutil code is polluted by
    non-mainline SchedTune code.
    
    Wrap SchedTune API call related to cpu utilization boosting with a more
    generic and mainlinish UtilClamp call:
    
     - uclamp_rq_util_with(cpu, util, p)  <= boosted_cpu_util(cpu)
    
    This new API is already used in schedutil_cpu_util() to clamp the
    aggregated RT+CFS utilization on FREQUENCY_UTIL calls.
    
    Move the cpu boosting into uclamp_rq_util_with() so that we remove any
    SchedTune specific bit from kernel/sched/cpufreq_schedutil.c.
    
    Get rid of the no more required boosted_cpu_util(cpu) method and replace
    it with a stune_util(cpu, util) which signature is better aligned with
    its uclamp_rq_util_with(cpu, util, p) counterpart.
    
    Bug: 120440300
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: I45b0f0f54123fe0a2515fa9f1683842e6b99234f
    [Removed superfluous __maybe_unused for capacity_orig_of]
    Signed-off-by: Quentin Perret <qperret@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6594,15 +6594,20 @@ schedtune_margin(unsigned long signal, long boost)
 	return margin;
 }
 
-static inline int
-schedtune_cpu_margin(unsigned long util, int cpu)
+inline long
+schedtune_cpu_margin_with(unsigned long util, int cpu, struct task_struct *p)
 {
-	int boost = schedtune_cpu_boost(cpu);
+	int boost = schedtune_cpu_boost_with(cpu, p);
+	long margin;
 
 	if (boost == 0)
-		return 0;
+		margin = 0;
+	else
+		margin = schedtune_margin(util, boost);
 
-	return schedtune_margin(util, boost);
+	trace_sched_boost_cpu(cpu, util, margin);
+
+	return margin;
 }
 
 static inline long
@@ -6623,8 +6628,8 @@ schedtune_task_margin(struct task_struct *task)
 
 #else /* CONFIG_SCHED_TUNE */
 
-static inline int
-schedtune_cpu_margin(unsigned long util, int cpu)
+inline long
+schedtune_cpu_margin_with(unsigned long util, int cpu, struct task_struct *p)
 {
 	return 0;
 }
@@ -6636,17 +6641,6 @@ schedtune_task_margin(struct task_struct *task)
 }
 
 #endif /* CONFIG_SCHED_TUNE */
-
-unsigned long
-boosted_cpu_util(int cpu)
-{
-	unsigned long util = cpu_util(cpu);
-	long margin = schedtune_cpu_margin(util, cpu);
-
-	trace_sched_boost_cpu(cpu, util, margin);
-
-	return util + margin;
-}
 
 static inline unsigned long
 boosted_task_util(struct task_struct *task)
