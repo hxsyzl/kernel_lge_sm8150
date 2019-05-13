fdca0430d7c878d002a616f1c2b86d361cdb16eb
Tejun Heo <tj@kernel.org>
2019-05-13 12:37:17 -0700

    UPSTREAM: cgroup: add cgroup_parse_float()
    cgroup already uses floating point for percent[ile] numbers and there
    are several controllers which want to take them as input.  Add a
    generic parse helper to handle inputs.
    
    Update the interface convention documentation about the use of
    percentage numbers.  While at it, also clarify the default time unit.
    
    Bug: 120440300
    Signed-off-by: Tejun Heo <tj@kernel.org>
    (cherry picked from commit a5e112e6424adb77d953eac20e6936b952fd6b32)
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: Ic1fcf21d7955eb8edd2e8e91517bca6aef41694f
    Signed-off-by: Quentin Perret <qperret@google.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -6251,7 +6251,6 @@ int cgroup_bpf_detach(struct cgroup *cgrp, struct bpf_prog *prog,
 #endif /* CONFIG_CGROUP_BPF */
 
 #ifdef CONFIG_SYSFS
-
 static u64 power_of_ten(int power)
 {
 	u64 v = 1;
@@ -6358,4 +6357,5 @@ static int __init cgroup_sysfs_init(void)
 	return sysfs_create_group(kernel_kobj, &cgroup_sysfs_attr_group);
 }
 subsys_initcall(cgroup_sysfs_init);
+
 #endif /* CONFIG_SYSFS */
