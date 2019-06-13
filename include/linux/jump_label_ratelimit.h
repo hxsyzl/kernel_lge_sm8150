762257c29b786e6d1227935a98491ee6acd8dfe1
Willem de Bruijn <willemb@google.com>
2019-06-13 11:08:15 -0400

    locking/static_key: always define static_branch_deferred_inc
    This interface is currently only defined if CONFIG_JUMP_LABEL. Make it
    available also when jump labels are off.
    
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- include/linux/jump_label_ratelimit.h
+++ include/linux/jump_label_ratelimit.h
@@ -19,6 +19,26 @@ extern void static_key_deferred_flush(struct static_key_deferred *key);
 extern void
 jump_label_rate_limit(struct static_key_deferred *key, unsigned long rl);
 
+extern void jump_label_update_timeout(struct work_struct *work);
+
+#define DEFINE_STATIC_KEY_DEFERRED_TRUE(name, rl)			\
+	struct static_key_true_deferred name = {			\
+		.key =		{ STATIC_KEY_INIT_TRUE },		\
+		.timeout =	(rl),					\
+		.work =	__DELAYED_WORK_INITIALIZER((name).work,		\
+						   jump_label_update_timeout, \
+						   0),			\
+	}
+
+#define DEFINE_STATIC_KEY_DEFERRED_FALSE(name, rl)			\
+	struct static_key_false_deferred name = {			\
+		.key =		{ STATIC_KEY_INIT_FALSE },		\
+		.timeout =	(rl),					\
+		.work =	__DELAYED_WORK_INITIALIZER((name).work,		\
+						   jump_label_update_timeout, \
+						   0),			\
+	}
+
 #else	/* !HAVE_JUMP_LABEL */
 struct static_key_deferred {
 	struct static_key  key;
@@ -39,4 +59,7 @@ jump_label_rate_limit(struct static_key_deferred *key,
 	STATIC_KEY_CHECK_USE();
 }
 #endif	/* HAVE_JUMP_LABEL */
+
+#define static_branch_deferred_inc(x)	static_branch_inc(&(x)->key)
+
 #endif	/* _LINUX_JUMP_LABEL_RATELIMIT_H */
