e2e39aa569f50d299c3d90a278b55f8a7926060a
SGCMarkus <markusornik@gmail.com>
2020-02-20 15:14:24 +0100

    drivers: input: touchscreen: lge: handle recovery mode the same as normal boot
    * caused to disable touchscreen when booting to custom recovery with "adb reboot bootloader" or rebooting to recovery within TWRP, while booting recovery with factory reset button combo had enabled touchscreen

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -789,8 +789,7 @@ static int touch_notify(struct touch_core_data *ts,
 
 	boot_mode = touch_check_boot_mode(ts->dev);
 	if (boot_mode == TOUCH_CHARGER_MODE
-			|| boot_mode == TOUCH_LAF_MODE
-			|| boot_mode == TOUCH_RECOVERY_MODE) {
+			|| boot_mode == TOUCH_LAF_MODE) {
 		TOUCH_I("%s: boot_mode = %d\n", __func__, boot_mode);
 		return 0;
 	}
@@ -863,8 +862,7 @@ static int display_notify(struct touch_core_data *ts,
 
 	boot_mode = touch_check_boot_mode(ts->dev);
 	if (boot_mode == TOUCH_CHARGER_MODE
-			|| boot_mode == TOUCH_LAF_MODE
-			|| boot_mode == TOUCH_RECOVERY_MODE) {
+			|| boot_mode == TOUCH_LAF_MODE) {
 		TOUCH_I("%s: boot_mode = %d\n", __func__, boot_mode);
 		return 0;
 	}
@@ -1322,8 +1320,7 @@ static int touch_core_probe(struct platform_device *pdev)
 
 	boot_mode = touch_check_boot_mode(ts->dev);
 	if (boot_mode == TOUCH_CHARGER_MODE
-			|| boot_mode == TOUCH_LAF_MODE
-			|| boot_mode == TOUCH_RECOVERY_MODE) {
+			|| boot_mode == TOUCH_LAF_MODE) {
 		TOUCH_I("%s: boot_mode = %d\n", __func__, boot_mode);
 		return touch_core_probe_etc(pdev);
 	}
