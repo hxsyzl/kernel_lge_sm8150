9981e9fe3ae895b76cb575a89ad912c67289a784
Juhyung Park <qkrwngud825@gmail.com>
2021-09-25 19:49:17 +0900

    BACKPORT: sched: promote nodes out of CONFIG_SCHED_DEBUG
    - This patch was backported from [1].
    
    [1]: https://github.com/arter97/android_kernel_oneplus_sm8350/commit/0ed5b00f732e8ee71e8c10e9df169fbc80ca0819
    
    [cyberknight777: backport to 4.14]
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -325,7 +325,6 @@ static struct ctl_table sysctl_base_table[] = {
 	{ }
 };
 
-#ifdef CONFIG_SCHED_DEBUG
 static int min_sched_granularity_ns = 100000;		/* 100 usecs */
 static int max_sched_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_wakeup_granularity_ns;			/* 0 usecs */
@@ -334,7 +333,6 @@ static int max_wakeup_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
 #endif /* CONFIG_SMP */
-#endif /* CONFIG_SCHED_DEBUG */
 
 #ifdef CONFIG_COMPACTION
 static int min_extfrag_threshold;
@@ -472,16 +470,6 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_updown_migrate_handler,
 	},
 	{
-		.procname       = "sched_energy_aware",
-		.data           = &sysctl_sched_energy_aware,
-		.maxlen         = sizeof(unsigned int),
-		.mode           = 0644,
-		.proc_handler   = proc_dointvec_minmax,
-		.extra1         = &zero,
-		.extra2         = &one,
-	},
-#ifdef CONFIG_SCHED_DEBUG
-	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
 		.maxlen		= sizeof(unsigned int),
@@ -539,6 +527,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
+#ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_nr_migrate",
 		.data		= &sysctl_sched_nr_migrate,
