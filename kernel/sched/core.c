538ff2233b41d7f8029386393a50d6cc47ca2de4
Andrzej Perczak <linux@andrzejperczak.com>
2021-12-16 19:58:25 +0100

    rcu: Squash backport from v5.4
    This is a shameless squash of Jebaitedeneko work:
    https://github.com/Jebaitedneko/android_kernel_xiaomi_vayu/tree/rcu
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6099,6 +6099,7 @@ int __sched _cond_resched(void)
 		preempt_schedule_common();
 		return 1;
 	}
+	rcu_all_qs();
 	return 0;
 }
 EXPORT_SYMBOL(_cond_resched);
@@ -6422,6 +6423,7 @@ void sched_show_task(struct task_struct *p)
 	show_stack(p, NULL);
 	put_task_stack(p);
 }
+EXPORT_SYMBOL_GPL(sched_show_task);
 
 static inline bool
 state_filter_match(unsigned long state_filter, struct task_struct *p)
@@ -7204,10 +7206,6 @@ int sched_cpu_deactivate(unsigned int cpu)
 	 *
 	 * Do sync before park smpboot threads to take care the rcu boost case.
 	 */
-
-#ifdef CONFIG_PREEMPT
-	synchronize_sched();
-#endif
 	synchronize_rcu();
 
 #ifdef CONFIG_SCHED_SMT
