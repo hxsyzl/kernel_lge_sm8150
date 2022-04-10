d47a042c52760279e5d68a0848b77b16bccfdef3
Alexander Winkowski <dereference23@outlook.com>
2022-04-10 15:46:17 +0000

    sysctl: Add dummy nodes for QTI perf HAL
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -335,10 +335,22 @@ static int min_extfrag_threshold;
 static int max_extfrag_threshold = 1000;
 #endif
 
+unsigned int dummy_sysctl_sched_boost;
+#ifndef CONFIG_SCHED_DEBUG
+unsigned int dummy_sysctl_sched_sync_hint_enable = 1;
+#endif
+
 static struct ctl_table kern_table[] = {
 	{
 		.procname	= "sched_child_runs_first",
 		.data		= &sysctl_sched_child_runs_first,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+	{
+		.procname	= "sched_boost",
+		.data		= &dummy_sysctl_sched_boost,
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
@@ -469,6 +481,14 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &one,
 	},
 #endif /* CONFIG_NUMA_BALANCING */
+#else /* CONFIG_SCHED_DEBUG */
+	{
+		.procname       = "sched_sync_hint_enable",
+		.data           = &dummy_sysctl_sched_sync_hint_enable,
+		.maxlen         = sizeof(unsigned int),
+		.mode           = 0644,
+		.proc_handler   = proc_dointvec,
+	},
 #endif /* CONFIG_SCHED_DEBUG */
 	{
 		.procname	= "sched_rt_period_us",
