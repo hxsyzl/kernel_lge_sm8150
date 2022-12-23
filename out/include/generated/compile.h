5344a193ca12159b11eecaab8b8612eed0d405d8
ShivamKumarJha <jha.shivam3@gmail.com>
2022-12-23 11:49:04 +0700

    Add arch/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#7 SMP PREEMPT Sat Apr 5 12:13:24 CST 2025"
+#define LINUX_COMPILE_BY "knbn"
+#define LINUX_COMPILE_HOST "DESKTOP-94F2IQA"
+#define LINUX_COMPILER "ZyC clang version 20.0.0git (https://github.com/llvm/llvm-project ba3e6f0f0f2bebeb2b82e976ea1b5df007784862), LLD 20.0.0 (https://github.com/llvm/llvm-project ba3e6f0f0f2bebeb2b82e976ea1b5df007784862)"
