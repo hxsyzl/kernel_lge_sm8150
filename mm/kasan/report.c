7797aa65885731166d2caffe1cbdfeac33c0c0d3
ShivamKumarJha <jha.shivam3@gmail.com>
2022-12-23 11:49:07 +0700

    Add mm/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- mm/kasan/report.c
+++ mm/kasan/report.c
@@ -91,7 +91,12 @@ static void end_report(unsigned long *flags)
 	pr_err("==================================================================\n");
 	add_taint(TAINT_BAD_PAGE, LOCKDEP_NOW_UNRELIABLE);
 	spin_unlock_irqrestore(&report_lock, *flags);
-	check_panic_on_warn("KASAN");
+#ifdef CONFIG_MACH_LGE
+	BUG_ON(1);
+#else
+	if (panic_on_warn)
+		panic("panic_on_warn set ...\n");
+#endif
 	kasan_enable_current();
 }
 
