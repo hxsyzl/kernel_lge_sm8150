4bee327805cc1f3262096d9ac1e1f3923e52fbc8
Anh Tran <anh.tran.3889@gmail.com>
2023-02-28 02:55:43 +0000

    Revert "rpmsg: glink: Remove IRQF_NO_SUSPEND"
    This reverts commit 999d0f74a6f012f32eccda5efd8ed41868a12085.

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -2120,6 +2120,8 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 
 	glink->irq = irq;
 
+	
+
 	size = of_property_count_u32_elems(dev->of_node, "cpu-affinity");
 	if (size > 0) {
 		arr = kmalloc_array(size, sizeof(u32), GFP_KERNEL);
