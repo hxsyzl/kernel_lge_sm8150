9d7f2c8ac3721f7b7438b26f66495bc05b648465
Xuewen Yan <xuewen.yan@unisoc.com>
2025-02-19 17:37:47 +0800

    BACKPORT: sched/uclamp: Optimize sched_uclamp_used static key enabling
    Repeat calls of static_branch_enable() to an already enabled
    static key introduce overhead, because it calls cpus_read_lock().
    
    Users may frequently set the uclamp value of tasks, triggering
    the repeat enabling of the sched_uclamp_used static key.
    
    Optimize this and avoid repeat calls to static_branch_enable()
    by checking whether it's enabled already.
    
    [ mingo: Rewrote the changelog for legibility ]
    
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Christian Loehle <christian.loehle@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20250219093747.2612-2-xuewen.yan@unisoc.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1250,12 +1250,12 @@ int sysctl_sched_uclamp_handler(struct ctl_table *table, int write,
 	}
 
 	if (update_root_tg) {
-		static_branch_enable(&sched_uclamp_used);
+		sched_uclamp_enable();
 		uclamp_update_root_tg();
 	}
 
 	if (old_min_rt != sysctl_sched_uclamp_util_min_rt_default) {
-		static_branch_enable(&sched_uclamp_used);
+		sched_uclamp_enable();
 		uclamp_sync_util_min_rt_default();
 	}
 
@@ -1315,7 +1315,7 @@ static int uclamp_validate(struct task_struct *p,
 	 * unconditionally holds the cpus_read_lock() which might_sleep().
 	 */
 	if (user)
-		static_branch_enable(&sched_uclamp_used);
+		sched_uclamp_enable();
 
 	return 0;
 }
@@ -7771,7 +7771,7 @@ static ssize_t cpu_uclamp_write(struct kernfs_open_file *of, char *buf,
 	if (req.ret)
 		return req.ret;
 
-	static_branch_enable(&sched_uclamp_used);
+	sched_uclamp_enable();
 
 	mutex_lock(&uclamp_mutex);
 	rcu_read_lock();
