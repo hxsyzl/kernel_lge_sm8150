89032918970bc220a5c0b0c79a62c62c743e0adf
hxsyzl <knbn666@gmail.com>
2025-05-03 09:43:45 +0800

    del don't use out

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 8 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#8 SMP PREEMPT Sat Apr 19 21:00:01 CST 2025"
-#define LINUX_COMPILE_BY "knbn"
-#define LINUX_COMPILE_HOST "DESKTOP-94F2IQA"
-#define LINUX_COMPILER "ZyC clang version 20.0.0git (https://github.com/llvm/llvm-project ba3e6f0f0f2bebeb2b82e976ea1b5df007784862), GNU ld (GNU Binutils) 2.44.50.20250123"
