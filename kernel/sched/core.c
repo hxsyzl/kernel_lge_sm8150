b95e78c617a0680a090590783156a237c9c39f00
hxsyzl <knbn666@gmail.com>
2025-08-30 17:25:40 +0800

    Optimize Android kernel's memory management and other
    Switch configuration files from Simple Low Memory Killer (LMK) to standard Android LMK, and enable automatic detection of OOM adj values (arch/arm64/configs/alpha_defconfig, flash_defconfig)
    Update KernelSU submodule to new version (a670b82)
    Fix uclamp variable declaration syntax (kernel/sched/core.c)

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -814,7 +814,7 @@ static unsigned int __maybe_unused sysctl_sched_uclamp_util_max = SCHED_CAPACITY
  * This knob will not override the system default sched_util_clamp_min defined
  * above.
  */
-static unsigned int sysctl_sched_uclamp_util_min_rt_default = 0;
+static unsigned int sysctl_sched_uclamp_util_min_rt_default;
 
 /* All clamps are required to be less or equal than these values */
 static struct uclamp_se uclamp_default[UCLAMP_CNT];
