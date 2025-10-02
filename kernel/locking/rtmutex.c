5b6f69791f1633d1bbbfc245f45cf0aad9f9567c
hxsyzl <knbn666@gmail.com>
2025-10-02 10:08:38 +0800

    fix

--- kernel/locking/rtmutex.c
+++ kernel/locking/rtmutex.c
@@ -1271,7 +1271,7 @@ rt_mutex_slowlock(struct rt_mutex *lock, int state,
 	if (unlikely(ret)) {
 		__set_current_state(TASK_RUNNING);
 		remove_waiter(lock, &waiter);
-		rt_mutex_handle_deadlock(ret, chwalk, &waiter);
+		rt_mutex_handle_deadlock(ret, chwalk, lock, &waiter);
 	}
 
 	/*
