d88fb30010279ceb4fee0366a4a75c4747f44d67
hxsyzl <knbn666@gmail.com>
2025-10-26 10:52:49 +0800

    Revert "sched: update irq util tracking"
    This reverts commit b7edeb2a9dd1162ab1eaab4eb0c217003389b0d3.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -936,10 +936,8 @@ struct rq {
 	u64 age_stamp;
 	u64 idle_stamp;
 	u64 avg_idle;
-#if defined(CONFIG_IRQ_TIME_ACCOUNTING) || \
-	defined(CONFIG_PARAVIRT_TIME_ACCOUNTING)
 	struct sched_avg avg_irq;
-#endif
+
 	/* This is used to determine avg_idle's max value */
 	u64 max_idle_balance_cost;
 #endif
@@ -1994,8 +1992,6 @@ static inline int hrtick_enabled(struct rq *rq)
 
 #endif /* CONFIG_SCHED_HRTICK */
 
-extern int update_irq_load_avg(struct rq *rq, u64 running);
-
 #ifdef CONFIG_SCHED_WALT
 u64 sched_ktime_clock(void);
 #else
@@ -2213,13 +2209,12 @@ cpu_util_freq(int cpu, struct sched_walt_cpu_load *walt_load)
 
 #else
 
-#if defined(CONFIG_IRQ_TIME_ACCOUNTING) || \
-	defined(CONFIG_PARAVIRT_TIME_ACCOUNTING)
-static inline unsigned long cpu_util_irq(struct rq *rq)
+static inline unsigned long cpu_util_irq(int cpu)
 {
+	struct rq *rq = cpu_rq(cpu);
+
 	return rq->avg_irq.util_avg;
 }
-#endif
 
 static inline unsigned long cpu_util_rt(int cpu)
 {
