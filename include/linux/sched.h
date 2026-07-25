d3b734b81f68808ee5ab06d0655206f6f535b225
hxsyzl <knbn666@gmail.com>
2026-07-25 15:41:02 +0800

    sched: Convert task_struct::usage to refcount_t squash https://github.com/sakana164/kernel_xiaomi_sm8250/commit/e7e350834cc37f0a716ffc750aa877c9049300b6 https://github.com/sakana164/kernel_xiaomi_sm8250/commit/4ba796c537da37d1e5f6308f9eb6a307f6ffa0a9

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -27,6 +27,7 @@
 #include <linux/signal_types.h>
 #include <linux/mm_types_task.h>
 #include <linux/task_io_accounting.h>
+#include <linux/refcount.h>
 
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
@@ -750,7 +751,7 @@ struct task_struct {
 	randomized_struct_fields_start
 
 	void				*stack;
-	atomic_t			usage;
+	refcount_t			usage;
 	/* Per task flags (PF_*), defined further below: */
 	unsigned int			flags;
 	unsigned int			ptrace;
